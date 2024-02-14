20bcf174ee8100359ea368a440620411e6890f64
Shubhankar Yadav <shubhankaryadav08@gmail.com>
2024-02-14 21:25:40 +0530

    Operators

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,8 +1,8 @@
 console.log("Hello World!")
 console.log("Welcome to Javascript")
-let a = 10;
-let b = 20;
-console.log("sum is :", a + b);
+let p = 10;
+let q = 20;
+console.log("sum is :", p + q);
 //this is a comment
 
 let pencilPrice = 10;
@@ -10,3 +10,25 @@ let erasorPrice = 5;
 // let output = "The total price is :" (pencilPrice + erasorPrice) + " Rupees. ";
 let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`; //back ticks
 console.log(output);
+
+//Arithmetic Operators
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+//Unary
+console.log(a++); //10
+console.log(++a); //12
+
+//Operators
+let w = 10;
+let e = 5;
+e = w;
+console.log(e); //10
+
+
