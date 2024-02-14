b0deb1c880ec393dbdd48801425f82055c5a0398
Shubhankar Yadav <shubhankaryadav08@gmail.com>
2024-02-14 20:46:01 +0530

    Template Literals
    Embedded expressions in String

---
+++ JavaScript/app.js
@@ -0,0 +1,12 @@
+console.log("Hello World!")
+console.log("Welcome to Javascript")
+let a = 10;
+let b = 20;
+console.log("sum is :", a + b);
+//this is a comment
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+// let output = "The total price is :" (pencilPrice + erasorPrice) + " Rupees. ";
+let output = `The total price is : ${pencilPrice + erasorPrice} Rupees.`; //back ticks
+console.log(output);
