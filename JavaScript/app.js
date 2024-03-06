c8c740bf9912406917c3cc1f1a5693569e13993a
Shubhankar Yadav <shubhankaryadav08@gmail.com>
2024-03-06 23:42:24 +0530

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -31,5 +31,34 @@ let e = 5;
 e = w;
 console.log(e); //10
 
+//comparsion
 let age = 16;
 console.log(age < 18); //false
+
+//for non-number (Uni code)
+'a' > 'A'
+//true
+'a' > 'b'
+//false
+
+//conditional statements
+
+//if statement
+console.log("before my if statement");
+let years = 21;
+if (years >= 18) {
+    console.log("you can vote")
+    console.log("you can drive")
+    let a = 5;
+    console.log(5 * a);
+}
+if(age < 18) {
+    console.log("you are in your 20s")
+    console.log("You cannot vote!")
+}
+console.log("after my if statement")
+
+let firstname = "shubhankar";
+if(firstname == "shubhankar") {
+    console.log(`Welcome ${firstname}`)
+}
